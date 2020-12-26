fcdcd6784b6dd2c4bf43128e108dff5f81d8026f
RakibulRanak <rakibulhasanranak1@gmail.com>
2020-12-26 23:33:26 +0600

    phase1copytophase2

--- controllers/AuthController.go
+++ controllers/AuthController.go
@@ -13,5 +13,5 @@ func NewAuthController() *AuthController {
 func (h *AuthController) LoginUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(201)
-	w.Write([]byte(`{"message" : "This function is not implemented Yet"}`))
+	w.Write([]byte(`{"message" : "This function  is not implemented Yet"}`))
 }
